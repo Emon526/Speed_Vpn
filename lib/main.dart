a0475d3e3014514beb1ba7e5e5f9bf5cb7077c86
Rizvi <>
2021-08-15 22:03:32 +0600

    browser new

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:get/get_navigation/get_navigation.dart';
 import 'package:speed_vpn/controllers/splash/splash_controller_binding.dart';
 import 'package:speed_vpn/screens/browser/browser.dart';
 import 'package:speed_vpn/screens/home/home_screen.dart';
+
 import 'package:speed_vpn/screens/splash/splash_screen.dart';
 
 void main(List<String> args) {
