02120d75f1d133bf5cf9150d41932dbb9c177f19
Rizvi <>
2021-08-15 15:33:32 +0600

    browser complete

--- lib/screens/browser/browser.dart
+++ lib/screens/browser/browser.dart
@@ -1,10 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
 
 class Browser extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text("data"),
+    return Scaffold(
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(15),
+        child: AppBar(
+          backgroundColor: Colors.white,
+        ),
+      ),
+      body: WebviewScaffold(
+        url: "https://google.com",
+        withZoom: false,
+      ),
     );
   }
 }
